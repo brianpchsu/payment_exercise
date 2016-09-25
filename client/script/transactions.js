296bc8a2390d1efa900f1229e0ec2bbcc6c61b6d
brianpchsu <brianpchsu@gmail.com>
2016-09-25 00:45:31 -0700

    Update API and Transaction history page

--- client/script/transactions.js
+++ client/script/transactions.js
@@ -11,11 +11,27 @@ function getTransaction() {
   var transactionReq = new XMLHttpRequest();
   transactionReq.onreadystatechange = function() {
     if(transactionReq.readyState === 4 && transactionReq.status === 200) {
-      var transactionArr = JSON.parse(transactionReq.responseText);
-      console.log('transactionArr from request is', transactionArr);
+      var allTransactions = JSON.parse(transactionReq.responseText);
+      console.log('transactionArr from request is', allTransactions);
+      showTransactions(allTransactions);
     }
   }
 
-  transactionReq.open("GET", "/getTransactions", true);
+  transactionReq.open('GET', '/getTransactions', true);
   transactionReq.send();
+}
+
+function showTransactions(allTransactions) {
+  var targetTable = document.getElementById('transactionTable');
+  var targetLen = allTransactions.length;
+  for(var i = 0; i < targetLen; i++) {
+    var tableRow = targetTable.insertRow(0);
+    var cellTime = tableRow.insertCell(0);
+    var cellRecipient = tableRow.insertCell(1);
+    var cellAmount = tableRow.insertCell(2);
+
+    cellTime.innerHTML = new Date(allTransactions[i].timestamp).toLocaleDateString();
+    cellRecipient.innerHTML = allTransactions[i].name;
+    cellAmount.innerHTML = allTransactions[i].amount;
+  }
 }
