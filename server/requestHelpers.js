8a50aabccf0064031c44578edc0013e0008eb349
brianpchsu <brianpchsu@gmail.com>
2016-09-24 23:29:37 -0700

    Use cache to save extra http calls

--- server/requestHelpers.js
+++ server/requestHelpers.js
@@ -1,19 +1,29 @@
 'use strict';
 
 var request = require('request');
+var transactionCache;
 
 function getTransactions(req, res) {
   console.log('call getTransactions');
-  request.get('http://beta.json-generator.com/api/json/get/4kFdw7gpb', function (error, response, body) {
-    if (error) {
-      console.error('Request transaction failed! Error is: ', error);
-      res.status(404).end();
-    }
-    if (!error && response.statusCode == 200) {
-      res.setHeader('Content-Type', 'application/json');
-      res.send(body);
-    }
-  })
+  if (transactionCache) {
+    res.send(transactionCache);
+  } else {
+    request.get('http://beta.json-generator.com/api/json/get/4kFdw7gpb',
+      sendTransactionResponse.bind(undefined, res));
+  }
+}
+
+function sendTransactionResponse(res, error, response, body) {
+  console.log('make http call to transaction');
+  if (error) {
+    console.error('Request transaction failed! Error is: ', error);
+    res.status(404).end();
+  }
+  if (!error && response.statusCode == 200) {
+    transactionCache = body;
+    res.setHeader('Content-Type', 'application/json');
+    res.send(body);
+  }
 }
 
 exports.getTransactions = getTransactions;
