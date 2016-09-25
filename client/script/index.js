702d721dbfcf42950f85bbc3f1542f50d618221f
brianpchsu <brianpchsu@gmail.com>
2016-09-24 22:10:53 -0700

    Add event listeners for buttons

---
+++ client/script/index.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+  document.getElementById('sendMoney').onclick=function(){
+    window.location = '/sendMoney.html';
+  };
+  document.getElementById('transHistory').onclick=function(){
+    window.location = '/transactions.html';
+  };
+};
---
+++ client/script/sendMoney.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+  document.getElementById('clearInput').onclick = function(){
+    // get all the element and clear all their content
+    document.getElementById('moneyForm').reset();
+  };
+  document.getElementById('transferMoney').onclick = function(){
+    // post form to server
+  };
+};
