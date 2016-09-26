9325f42c06450bfedb760fa6a7b1691d095b4471
brianpchsu <brianpchsu@gmail.com>
2016-09-25 17:38:28 -0700

    Implement check email for sendMoney

--- client/script/sendMoney.js
+++ client/script/sendMoney.js
@@ -3,7 +3,34 @@ window.onload = function(){
     // get all the element and clear all their content
     document.getElementById('moneyForm').reset();
   };
+
   document.getElementById('transferMoney').onclick = function(){
     // post form to server
   };
+
+  // check valid email
+  var emailInput = document.moneyForm.recipient;
+  emailInput.onchange = function() {
+    var email = emailInput.value;
+    console.log('email is ', email);
+
+    if(checkValidEmail(email)) {
+      console.log('valid email');
+      emailInput.className = 'validInput';
+      var checkMarkIcon = document.getElementById('checkmark');
+      checkMarkIcon.style.visibility = 'visible';
+    } else {
+      console.log('Invalid email');
+      emailInput.className = 'invalidInput';
+      var checkMarkIcon = document.getElementById('checkmark');
+      checkMarkIcon.style.visibility = 'hidden';
+    }
+  };
+
 };
+
+// Use regular expressions to check email, ref: http://stackoverflow.com/a/46181/3525493
+function checkValidEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
